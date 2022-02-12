8f8ab3a24ab2c641e81cd743e900eace36e4dda8
Adrian Baddeley <adrian.baddeley@curtin.edu.au>
2022-02-12 16:35:24 +0800

    aaarggghhhh

--- R/clusterinfo.R
+++ R/clusterinfo.R
@@ -655,7 +655,7 @@
            }
            ## compute indefinite integral
            imethod <- if(spatstat.options("fastK.lgcp")) "trapezoid" else "quadrature"
-           th <- indefinteg(integrand, rvals, from=0, method=imethod)
+           th <- indefinteg(integrand, rvals, lower=0, method=imethod)
            return(th)
          },
          pcf= function(par, rvals, ..., model, margs) {
