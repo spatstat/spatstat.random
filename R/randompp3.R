1d9eba362f9cbf1af6be914d66b13c09629380e4
Adrian Baddeley <Adrian.Baddeley@curtin.edu.au>
2026-03-18 15:57:40 +0800

    inhomogeneous 3D Poisson process

--- R/randompp3.R
+++ R/randompp3.R
@@ -1,10 +1,16 @@
 #'
 #'   randompp3.R
 #'
-#'   $Revision: 1.2 $ $Date: 2022/05/23 02:33:06 $
+#'   $Revision: 1.4 $ $Date: 2026/03/18 07:46:31 $
 #'
 
-runifpoint3 <- function(n, domain=box3(), nsim=1, drop=TRUE) {
+runifpoint3 <- function(n, domain=box3(), ..., nsim=1, drop=TRUE, ex=NULL) {
+  if(!is.null(ex)) {
+    ## make a pattern like 'ex'
+    stopifnot(is.pp3(ex))
+    if(missing(n) || is.null(n)) n <- npoints(ex)
+    if(missing(domain) || is.null(domain)) domain <- domain(ex)
+  }
   domain <- as.box3(domain)
   check.1.integer(nsim)
   stopifnot(nsim >= 0)
@@ -22,22 +28,59 @@ runifpoint3 <- function(n, domain=box3(), nsim=1, drop=TRUE) {
   return(result)
 }
 
-rpoispp3 <- function(lambda, domain=box3(), nsim=1, drop=TRUE) {
+rpoispp3 <- function(lambda, domain=box3(), ...,
+                     nsim=1, drop=TRUE, ex=NULL, lmax=NULL) {
+  if(!is.null(ex)) {
+    stopifnot(is.pp3(ex))
+    if(missing(lambda) || is.null(lambda)) lambda <- intensity(unmark(ex))
+    if(missing(domain) || is.null(domain)) domain <- domain(ex)
+  }
   domain <- as.box3(domain)
   v <- volume(domain)
   check.1.integer(nsim)
   stopifnot(nsim >= 0)
-  if(!(is.numeric(lambda) && length(lambda) == 1))
-    stop("lambda must be a single numeric value")
-  np <- rpois(nsim, lambda * v)
-  dd <- as.list(domain)[c("xrange", "yrange", "zrange")]
-  result <- vector(mode="list", length=nsim)
-  for(i in seq_len(nsim)) {
-    ni <- np[i]
-    x <- with(dd, runif(ni, min=xrange[1], max=xrange[2]))
-    y <- with(dd, runif(ni, min=yrange[1], max=yrange[2]))
-    z <- with(dd, runif(ni, min=zrange[1], max=zrange[2]))
-    result[[i]] <- pp3(x,y,z,domain)
+  if(is.numeric(lambda) && length(as.numeric(lambda)) == 1) {
+    lmax <- as.numeric(lambda)
+  } else if(is.function(lambda)) {
+    if(is.null(lmax))
+      stop("lmax is required when lambda is a function", call.=FALSE)
+    check.1.real(lmax)
+  } else {    
+    stop("lambda must be a single numeric value, or a function(x,y,z")
+  }
+  inhom <- is.function(lambda)
+  if(lmax <= 0) {
+    ## zero intensity => empty patterns
+    none <- numeric(0)
+    emptypattern <- pp3(none, none, none, domain)
+    result <- rep(list(emptypattern), nsim)
+  } else {
+    ## generate homogeneous Poisson
+    np <- rpois(nsim, lmax * v)
+    dd <- as.list(domain)[c("xrange", "yrange", "zrange")]
+    result <- vector(mode="list", length=nsim)
+    probrange <- numeric(0)
+    for(i in seq_len(nsim)) {
+      ni <- np[i]
+      x <- with(dd, runif(ni, min=xrange[1], max=xrange[2]))
+      y <- with(dd, runif(ni, min=yrange[1], max=yrange[2]))
+      z <- with(dd, runif(ni, min=zrange[1], max=zrange[2]))
+      X.i <- pp3(x,y,z,domain)
+      if(inhom && ni > 0) {
+        ## Lewis-Shedler thinning 
+        p <- lambda(x,y,z)/lmax
+        probrange <- range(p, probrange)
+        retain <- (runif(ni) <= p)
+        X.i <- X.i[retain]
+      }
+      result[[i]] <- X.i
+    }
+    if(inhom && length(probrange)) {
+      if(probrange[1L] < 0)
+        warning("Negative values of lambda(x,y,z) occurred", call.=FALSE)
+      if(probrange[2L] > 1)
+        warning("Some values of lambda(x,y,z) exceeded lmax", call.=FALSE)
+    }
   }
   if(drop && nsim == 1) return(result[[1]])
   result <- as.anylist(result)
